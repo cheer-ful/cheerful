3391f33a1269ce155d37f0ed7ed2882b62237734
losiz17 <riokam.11.12@docomo.ne.jp>
2019-11-03 11:58:35 +0900

    add: とりあえずrankingに書いたけどあとで時間とか変えるかも

--- public/js/ranking.js
+++ public/js/ranking.js
@@ -4,54 +4,96 @@ var rank1Id = "";
 var rank2Id = "";
 var rank3Id = "";
 
+// function geo(position) {
+//     var geo_text = "緯度:" + position.coords.latitude + "\n";
+//     geo_text += "経度:" + position.coords.longitude + "\n";
+//     geo_text += "高度:" + position.coords.altitude + "\n";
+//     geo_text += "位置精度:" + position.coords.accuracy + "\n";
+//     geo_text += "高度精度:" + position.coords.altitudeAccuracy + "\n";
+//     geo_text += "移動方向:" + position.coords.heading + "\n";
+//     geo_text += "速度:" + position.coords.speed + "\n";
+//     var date = new Date(position.timestamp);
 
-// 位置情報
-// watch_id = navigator.geolocation.watchPosition(geo, function (e) { alert(e.message); }, { "enableHighAccuracy": true, "timeout": 20000, "maximumAge": 2000 });
+//     //サンプルデータ(オアシス21)
+//     var tLatitude = 35.1708924;
+//     var tLongitude = 136.9095254;
 
-function geo(position) {
-    var geo_text = "緯度:" + position.coords.latitude + "\n";
-    geo_text += "経度:" + position.coords.longitude + "\n";
-    geo_text += "高度:" + position.coords.altitude + "\n";
-    geo_text += "位置精度:" + position.coords.accuracy + "\n";
-    geo_text += "高度精度:" + position.coords.altitudeAccuracy + "\n";
-    geo_text += "移動方向:" + position.coords.heading + "\n";
-    geo_text += "速度:" + position.coords.speed + "\n";
-    var date = new Date(position.timestamp);
-    //サンプルデータ(オアシス21)
-    var tLatitude = 35.1708924;
-    var tLongitude = 136.9095254;
-    //サンプルデータと現時点との角度測定。
-    tLatitude *= Math.PI / 180;
-    tLongitude *= Math.PI / 180;
-    var gLatitude = position.coords.latitude * Math.PI / 180;
-    var gLongitude = position.coords.longitude * Math.PI / 180;
-    var Between = 0.0;
-    var time = 0.0;
-    //二点間の距離
-    Between += 6371 * Math.acos(Math.cos(tLatitude) * Math.cos(gLatitude) * Math.cos(gLongitude - tLongitude) + Math.sin(tLatitude) * Math.sin(gLatitude)) + "\n";
-    geo_text += "二点間の距離" + Between + "km" + "\n";
-    // console.log(Between);
-    //時間
-    time += ((6371 * Math.acos(Math.cos(tLatitude) * Math.cos(gLatitude) * Math.cos(gLongitude - tLongitude) + Math.sin(tLatitude) * Math.sin(gLatitude))) * 1000) / 77 + "\n";
-    geo_text += "かかる時間" + time + "分" + "\n";
-    // console.log(time);
-    // geo_text += "取得時刻:" + date.toLocaleString() + "\n";
-    geo_text += "更新回数:" + (++num) + "\n";
-    var rad = Math.atan2(gLatitude - tLatitude, gLongitude - tLongitude);
-    // var angle = rad * (180 / Math.PI) ; // ラジアンを度数へ変換
-    geo_text += "角度" + (rad * (180 / Math.PI) + 180);
-    console.log(rad);
-    document.getElementById('position_view').innerHTML = geo_text;
-    // return rad; //二点間角度
+//     //サンプルデータと現時点との角度測定。
+//     tLatitude *= Math.PI / 180;
+//     tLongitude *= Math.PI / 180;
+
+//     var Between = 0.0;
+//     var time = 0.0;
+
+//     //二点間の距離
+//     Between += 6371 * Math.acos(Math.cos(tLatitude) * Math.cos(gLatitude) * Math.cos(gLongitude - tLongitude) + Math.sin(tLatitude) * Math.sin(gLatitude)) + "\n";
+//     geo_text += "二点間の距離" + Between + "km" + "\n";
+//     // console.log(Between);
+
+//     //時間
+//     time += ((6371 * Math.acos(Math.cos(tLatitude) * Math.cos(gLatitude) * Math.cos(gLongitude - tLongitude) + Math.sin(tLatitude) * Math.sin(gLatitude))) * 1000) / 77 + "\n";
+//     geo_text += "かかる時間" + time + "分" + "\n";
+//     // console.log(time);
+//     // geo_text += "取得時刻:" + date.toLocaleString() + "\n";
+//     geo_text += "更新回数:" + (++num) + "\n";
+//     var rad = Math.atan2(gLatitude - tLatitude, gLongitude - tLongitude);
+//     // var angle = rad * (180 / Math.PI) ; // ラジアンを度数へ変換
+//     geo_text += "角度" + (rad * (180 / Math.PI) + 180);
+//     console.log(rad);
+//     document.getElementById('position_view').innerHTML = geo_text;
+//     // return rad; //二点間角度
+// }
+
+if (navigator.geolocation) {
+    // alert("この端末では位置情報が取得できます");
+    // Geolocation APIに対応していない
+} else {
+    alert("この端末では位置情報が取得できません");
 }
+
+function getTime(gLatitude, gLongitude, tLatitude, tLongitude) {
+    let time = 0;
+    time += ((6371 * Math.acos(Math.cos(tLatitude) * Math.cos(gLatitude) * Math.cos(gLongitude - tLongitude) + Math.sin(tLatitude) * Math.sin(gLatitude))) * 1000) / 77 + "\n";
+    return time;
+}
+
+navigator.geolocation.getCurrentPosition(
+    // 取得成功した場合
+    function (position) {
+        //glocal scope
+        var gLatitude = position.coords.latitude * Math.PI / 180;
+        var gLongitude = position.coords.longitude * Math.PI / 180;
+        alert("緯度:" + position.coords.latitude + ",経度" + position.coords.longitude);
+        let time = getTime(gLatitude, gLongitude)
+    },
+    // 取得失敗した場合
+    function (error) {
+        switch (error.code) {
+            case 1: //PERMISSION_DENIED
+                alert("位置情報の利用が許可されていません");
+                break;
+            case 2: //POSITION_UNAVAILABLE
+                alert("現在位置が取得できませんでした");
+                break;
+            case 3: //TIMEOUT
+                alert("タイムアウトになりました");
+                break;
+            default:
+                alert("その他のエラー(エラーコード:" + error.code + ")");
+                break;
+        }
+    }
+);
 
 // artistRef.push({ name: "name", point: 0, location_x: "", location_y: "", color: "green" });
 artistRef.on('value', function (snapshot) {
 
     valueArray = Object.values(snapshot.val());
+    console.log(valueArray[0].location_x);
+
     object_array_sort(valueArray, 'point', 'dec', function (new_data) {
         //ソート後の処理
-        console.log(new_data); //
+        console.log(new_data);
         // for (var i = 0; i < 10; i++) {
         //     document.write(new_data[i].name + "," + new_data[i].point + "<br>");
         // }
